f351ee5d60dcef932a24e68aba60b7d51d33f01d
ori-code <88030092+OriDoboshZippit@users.noreply.github.com>
2022-03-27 18:20:37 +0300

    new upload

---
+++ app/src/main/java/com/example/namaprojectfirebase/User.java
@@ -0,0 +1,14 @@
+package com.example.namaprojectfirebase;
+
+public class User {
+    public String fullName, adressText, email;
+
+    public User() {
+
+    }
+    public User(String fullName, String address, String email){
+        this.fullName = fullName;
+        this.adressText = address;
+        this.email = email;
+    }
+}
