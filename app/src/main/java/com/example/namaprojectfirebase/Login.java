7a0ad0c107e82e7af105d91275646c21e7ecbba4
OriDoboshZippit <ori.dobosh@zippit.net>
2022-05-04 11:43:20 +0300

    Drawer

--- app/src/main/java/com/example/namaprojectfirebase/Login.java
+++ app/src/main/java/com/example/namaprojectfirebase/Login.java
@@ -32,12 +32,11 @@ public class Login extends AppCompatActivity {
     private EditText editTextEmail, editTextPassword;
     private Button signIn;
     private String users,email,password;
-    private FirebaseAuth mAuth;
+    public FirebaseAuth mAuth;
     private FirebaseDatabase mDatabase;
     private ProgressBar progressBar;
     public String nameFromDB;
     public Query currentUser;
-//    ActivityReadDataBinding binding;
     DatabaseReference databaseReference;
 
     @Override
@@ -48,7 +47,6 @@ public class Login extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         editTextEmail = (EditText) findViewById (R.id.editTextTextEmailAddress);
         editTextPassword = (EditText) findViewById (R.id.editTextTextPassword);
-
         databaseReference = FirebaseDatabase.getInstance().getReference("users");
         currentUser = databaseReference.orderByChild("adressText");
     }
@@ -58,77 +56,53 @@ public class Login extends AppCompatActivity {
     public void loginFunc(View view) {
         email = editTextEmail.getText().toString().trim();
         password = editTextPassword.getText().toString().trim();
-//        userLogin();
-        mAuthFunc();
-        
-        //pull the current user
-        currentUser.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
+        System.out.println("The email is " + email + " and password " + password);
 
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                nameFromDB = dataSnapshot.child(mAuth.getCurrentUser().getUid()).child("fullName").getValue(String.class);
-                if(dataSnapshot.exists()){
-                    System.out.println("Data snap shoot work" );
-                    System.out.println("Password is " +  nameFromDB);
-                }
-            }
+//        System.out.println(mAuth.signInWithEmailAndPassword(email, password).isSuccessful());
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    //redirect to the activity profile
+                    System.out.println("Yeeeeey we got in to the system with email !!!! " + mAuth.getCurrentUser().getEmail());
+                    Intent i = new Intent(Login.this, DrawerActivity.class);
+                    i.putExtra("id", nameFromDB);
+                    System.out.println(nameFromDB);
+//                    if(nameFromDB!=null) {
+                        startActivity(i);
+//                    }
+                } else {
+                    System.out.println("Yeeeeey we DONT got in to the system with name because mAuth dont works" );
+                    Toast.makeText(Login.this, "You need to try again to login", Toast.LENGTH_LONG).show();
+                }
+
 
             }
         });
-        System.out.println("IM in LOGIN");
+
+
+//        //pull the current user
+//        currentUser.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                nameFromDB = dataSnapshot.child(mAuth.getCurrentUser().getUid()).child("fullName").getValue(String.class);
+//                if(dataSnapshot.exists()){
+//                    System.out.println("Data snap shoot work" );
+//                    System.out.println("Password is " +  nameFromDB);
+//                }
+//            }
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//
+//        System.out.println("IM in LOGIN");
+//
+//
     }
 
-    //login function with data base
-    private void userLogin() {
-        System.out.println("email is " + email + "pass" + password);
-        if (email.isEmpty()) {
-            editTextEmail.setError("Email is required!");
-            editTextEmail.requestFocus();
-            return;
-        }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            editTextEmail.setError("Please enter a valid email!");
-            editTextEmail.requestFocus();
-            return;
-        }
-        if (password.isEmpty()) {
-            editTextPassword.setError("Password is required!");
-            editTextPassword.requestFocus();
-            return;
-        }
-        if (password.length() < 6) {
-            editTextPassword.setError("Min password length is 6 characters!");
-            editTextPassword.requestFocus();
-            return;
-        }
-
-    }
-
-    public void mAuthFunc() {
-        System.out.println("HHHHHEYYY "+ email + password);
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        //redirect to the activity profile
-                        System.out.println("Yeeeeey we got in to the system with name !!!! " + nameFromDB);
-                        Intent i = new Intent(Login.this, ProfileActivity.class);
-                        i.putExtra("id", nameFromDB);
-                        System.out.println(nameFromDB);
-                        if(nameFromDB!=null) {
-                            startActivity(i);
-                        }
-
-                    } else {
-                        System.out.println("Yeeeeey we DONT got in to the system with name because nameFromDb is " + nameFromDB);
-                        Toast.makeText(Login.this, "Try again ", Toast.LENGTH_LONG).show();
-                    }
-
-                }
-            });
-    }
 
 
 }
